85187b7126780c8273e5fca4e6ebec39b1fd0043
Tom <haveto.oshigoto@gmail.com>
2021-05-19 20:34:01 +0900

    update

--- src/popup.js
+++ src/popup.js
@@ -1,40 +1,17 @@
 ﻿// //----------------------
-//     console.log('popup.js');
-
-// // chrome.runtime.onMessage.addListener(
-// //     function(request, sender, callback) {  // 1
-// //         chrome.tabs.getSelected(function(tab) {  // 2
-// //             callback(tab.title);
-// //         });
-// //         // 3
-// //     }
-// // );
-// // chrome.runtime.onMessage.addListener(function(message, sender, sendResponse){
-// // 	console.log(message)
-// // 	setTimeout(function(){
-// // 		sendResponse({a:1,b:2})
-// // 	}, 5000)
-// // 	return true
-// // })
-// // 
-//   $("#btn").on("click", () => {
-//     chrome.runtime.sendMessage({ greeting: "hello" }, function(response) {
-//       console.log(response);
-//     });
-//   });
 console.log("popup.js");
 
-var textContents = chrome.extension.getBackgroundPage().textContents;
+var textTitle = chrome.extension.getBackgroundPage().textTitle;
 var textUrl = chrome.extension.getBackgroundPage().textUrl;
 var div = document.getElementById('textView');
 var url = document.getElementById('textUrl');
 var shortCode = document.getElementById('shortCode');
-div.textContent = textContents;
+div.textContent = textTitle;
 url.textContent = textUrl;
-shortCode.textContent = '[oblink_comment url="'+textUrl+'" title="'+textContents+'"][/oblink_comment]';
+// ショートコードに値を埋め込む
+shortCode.textContent = '[oblink_comment url="'+textUrl+'" title="'+textTitle+'"][/oblink_comment]';
 
-
-
+// コピー用関数
 function copy() {
   let range = document.createRange();
   var copyTarget = document.getElementById("shortCode");// コピー対象の要素を取得
@@ -42,50 +19,9 @@ function copy() {
   let selection = document.getSelection();
   selection.removeAllRanges();
   selection.addRange(range);
-  document.execCommand("copy");// クリップボードにコピー
+  document.execCommand("copy");
 }
 document.getElementById("shortCode").addEventListener("click", copy);
 
 
 
-
-
-
-
-chrome.runtime.onMessage.addListener(function(msg, sender, sendResponse) {
-  console.log(msg);//送られたメッセージをキャッチ
-  sendResponse(response);//sendResponseでmsgを送ったスクリプト側にレスを返せる
-});
-
-
-console.log("test");
-  $("#btn").on("click", () => {
-    chrome.runtime.sendMessage({ greeting: "hello" }, function(response) {
-      console.log(response);
-    });
-  chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
-    chrome.tabs.sendMessage(tabs[0].id, {
-      message: "これはテスト"
-    });
-  });
-
-  });
-
-$("#black").on("click", () => {
-  chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
-    chrome.tabs.sendMessage(tabs[0].id, {
-      color: "black"
-    });
-  });
-});
-
-$("#red").on("click", () => {
-  //送れない！！
-  // chrome.runtime.sendMessage({ color: "red" });
-
-  chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
-    chrome.tabs.sendMessage(tabs[0].id, {
-      color: "red"
-    });
-  });
-});
